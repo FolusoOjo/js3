d5c57304f9e2b1c0ca73792e38140787a67a97ed
FolusoOjo <98808180+FolusoOjo@users.noreply.github.com>
2022-05-18 09:02:09 +0100

    Add files via upload

---
+++ js3/index.js
@@ -0,0 +1,14 @@
+let musicalGroup = 4;
+if (musicalGroup <= 0) {
+  console.log("not a group");
+} else if (musicalGroup == 1) {
+  console.log("solo");
+} else if (musicalGroup == 2) {
+  console.log("duet");
+} else if (musicalGroup == 3) {
+  console.log("trio");
+} else if (musicalGroup == 4) {
+  console.log("quartet");
+} else {
+  console.log("This is a large group");
+}
